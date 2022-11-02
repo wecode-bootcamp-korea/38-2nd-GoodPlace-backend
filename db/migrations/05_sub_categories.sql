79a67744da197f2c7d315aafb64f2941d86ba100
jeonghaeman <haemong_2@naver.com>
2022-11-02 17:00:17 +0900

    modify migration

--- db/migrations/05_sub_categories.sql
+++ db/migrations/05_sub_categories.sql
@@ -2,7 +2,7 @@
 CREATE TABLE sub_categories (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NULL,
-    main_category_id INT NOT NULL
+    main_category_id INT NOT NULL,
     FOREIGN KEY (main_category_id) REFERENCES main_categories (id)
 )
 
